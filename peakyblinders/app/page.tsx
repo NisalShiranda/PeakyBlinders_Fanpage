75d9c95bd4f02fb10dd958a54f821d40bce247e7
Nisal Shiranda <nisalshiranda001@gmail.com>
2024-02-15 12:13:30 +0530

    ss

--- peakyblinders/app/page.tsx
+++ peakyblinders/app/page.tsx
@@ -3,8 +3,17 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <div className="bg-green">
-      Nisal Shiranda
+    <div className="hero-section relative">
+        <Image className='w-full '
+          src="/hero1.jpg"
+          width={5000000}
+          height={500000}
+          alt="Picture of the author"
+          quality={100}
+        />
+      <div className="absolute inset-0 flex items-center justify-center text-white text-center">
+        <p>Peaky Blinders</p>
+      </div>
     </div>
     
     
