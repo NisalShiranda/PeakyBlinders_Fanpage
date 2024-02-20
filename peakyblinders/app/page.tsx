56285bfe9c8342e2410c2f020148975b12a4cb74
Nisal Shiranda <nisalshiranda001@gmail.com>
2024-02-20 11:26:23 +0530

    p tag font and sizes

--- peakyblinders/app/page.tsx
+++ peakyblinders/app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
         />
       <div className=" absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center ">
         <p className="hero  text-gray-200 text-center text-5xl font-thin min-[375px]:text-6xl min-[425px]:text-7xl min-[500px]:text-8xl sm:text-9xl min-[900px]:text-[150px] lg:text-[200px] xl:text-[250px] 2xl:text-[300px] ">Peaky Blinders</p>
-        <p className=" text-xl  text-gray-400 text-center px-5">Welcome, Peaky Blinders enthusiasts, to a place where the razor-sharp style meets the gritty streets of Birmingham! This is your ultimate destination for all things Peaky Blinders, a fan page dedicated to celebrating the gripping saga of the Shelby family.</p>
-        <p className=" text-white text-sm">Nisal Shiranda</p>
+        <p className=" text-[10px] font-thin text-gray-400 text-center px-10 sm:text-md md:text-lg lg:text-xl xl:text-2xl ">Welcome, Peaky Blinders enthusiasts, to a place where the razor-sharp style meets the gritty streets of Birmingham! This is your ultimate destination for all things Peaky Blinders, a fan page dedicated to celebrating the gripping saga of the Shelby family.</p>
+        
       </div>
     </div>
     
