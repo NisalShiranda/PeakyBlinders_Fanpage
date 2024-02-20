67ee31c5ec912e77fba7a9fec193d6c7d8230f19
Nisal Shiranda <nisalshiranda001@gmail.com>
2024-02-20 07:14:28 +0530

    ss

--- peakyblinders/app/page.tsx
+++ peakyblinders/app/page.tsx
@@ -4,15 +4,16 @@ export default function Home() {
   return (
     <>
     <div className="hero-section relative">
-        <Image className='w-full'
+        <Image className='w-full bg-cover bg-center'
           src="/hero1.jpg"
           width={5000000}
           height={500000}
           alt="Picture of the author"
           quality={100}
         />
-      <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center text-white text-center text-xl">
-        <p>Peaky Blinders</p>
+      <div className=" absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center ">
+        <p className="hero  text-gray-200 text-center text-5xl font-thin min-[375px]:text-6xl min-[425px]:text-7xl min-[500px]:text-8xl sm:text-9xl min-[900px]:text-[150px] lg:text-[200px] xl:text-[250px] 2xl:text-[300px] ">Peaky Blinders</p>
+        <p className=" font-serif text-[5px]  text-gray-400 text-center px-5 ">Welcome, Peaky Blinders enthusiasts, to a place where the razor-sharp style meets the gritty streets of Birmingham! This is your ultimate destination for all things Peaky Blinders, a fan page dedicated to celebrating the gripping saga of the Shelby family.</p>
       </div>
     </div>
     
