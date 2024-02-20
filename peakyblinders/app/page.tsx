4f14b8645a78eb18f368aed570142ac3de9a4730
Nisal Shiranda <nisalshiranda001@gmail.com>
2024-02-20 20:19:38 +0530

    main cast card create

--- peakyblinders/app/page.tsx
+++ peakyblinders/app/page.tsx
@@ -26,9 +26,26 @@ export default function Home() {
           alt="Picture of the author"
           quality={100}
         />
-      <div className=" absolute inset-0 bg-black bg-opacity-75 flex flex-col items-center  ">
-        <p className="hero  text-gray-200 text-center text-5xl font-thin min-[375px]:text-6xl min-[425px]:text-7xl min-[500px]:text-8xl sm:text-9xl min-[900px]:text-[150px] lg:text-[200px] xl:text-[250px] 2xl:text-[300px] ">CAST</p>
-        <p className=" text-[10px] font-thin text-gray-400 text-center px-10 sm:text-md md:text-lg lg:text-xl xl:text-2xl ">Welcome, Peaky Blinders enthusiasts, to a place where the razor-sharp style meets the gritty streets of Birmingham! This is your ultimate destination for all things Peaky Blinders, a fan page dedicated to celebrating the gripping saga of the Shelby family.</p>
+      <div className=" absolute inset-0 bg-black bg-opacity-75  flex flex-col items-center justify-center  ">
+        <p className="hero  text-gray-200 text-center text-2xl  min-[375px]:text-3xl min-[425px]:text-4xl min-[500px]:text-5xl sm:text-6xl min-[900px]:text-7xl lg:text-8xl xl:text-9xl 2xl:text-[100px] ">MAIN CAST</p>
+        <div className="flex flex-row justify-center items-center gap-2 pt-1 md:gap-4 sm:pt-5 lg:gap-6 xl:gap-10 2xl:gap-14">
+          <div className="bg-white h-24 w-16 sm:w-24 sm:h-36 md:w-32 md:h-48 lg:w-44 lg:h-64 xl:w-52 xl:h-80 2xl:w-64 2xl:h-96 ">
+            <div>
+            <Image className=' p-0.5'
+            src="/p1.jpeg"
+            width={5000000}
+            height={500000}
+            alt="Picture of the author"
+            quality={100}
+            />
+            </div>
+            <div></div>
+          </div>
+          <div  className="bg-white h-24 w-16 sm:w-24 sm:h-36 md:w-32 md:h-48 lg:w-44 lg:h-64 xl:w-52 xl:h-80 2xl:w-64 2xl:h-96">2</div>
+          <div  className="bg-white h-24 w-16 sm:w-24 sm:h-36 md:w-32 md:h-48 lg:w-44 lg:h-64 xl:w-52 xl:h-80 2xl:w-64 2xl:h-96 ">3</div>
+          <div  className="bg-white h-24 w-16 sm:w-24 sm:h-36 md:w-32 md:h-48 lg:w-44 lg:h-64 xl:w-52 xl:h-80 2xl:w-64 2xl:h-96">4</div>
+        </div>
+        
         
       </div>
     </div>
